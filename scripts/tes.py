d0480c9d30417bec082743ca277b3821e21c46a2
Handiriz <handinugr@yahoo.co.id>
2021-07-06 19:50:39 +0700

    creates a scripts folder and a python script

---
+++ scripts/tes.py
@@ -0,0 +1,9 @@
+#This notebook is mainly created for the capstone project purpose in the future
+
+#Coding testing to see whether the notebook is working or not
+#import libraries
+import pandas as pd
+import numpy as np
+
+#tes print
+print('Hello Capstone Project Course!')
